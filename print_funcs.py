d21764a22d09ce2814e13df5e5311942e993a548
romakouz <67249384+romakouz@users.noreply.github.com>
2022-06-06 12:26:51 -0700

    Update print_funcs.py

--- print_funcs.py
+++ print_funcs.py
@@ -6,22 +6,22 @@
 
 #printpuzzle() function
 def print_puzzle(p):
-  '''
-  Takes input p a sudoku puzzle as a 9 by 9 numpy array,
-  and returns a string giving a realistic depiction of a sudoku puzzle
-  '''
-  puzzleprint = ""
-  for i in range(len(p)):
-      if i % 3 == 0 and i != 0:
-          puzzleprint += "------------------------ "
+    '''
+    Takes input p a sudoku puzzle as a 9 by 9 numpy array,
+    and returns a string giving a realistic depiction of a sudoku puzzle
+    '''
+    puzzleprint = ""
+    for i in range(len(p)):
+        if i % 3 == 0 and i != 0:
+            puzzleprint += "------------------------ "
 
-      for j in range(len(p[0])):
-          if j % 3 == 0 and j != 0:
-              puzzleprint += " | "
+        for j in range(len(p[0])):
+            if j % 3 == 0 and j != 0:
+                puzzleprint += " | "
 
-          if j == 8:
-              puzzleprint += str(p[i][j])
-          else:
-              puzzleprint += str(p[i][j]) + " "
+            if j == 8:
+                puzzleprint += str(p[i][j])
+            else:
+                puzzleprint += str(p[i][j]) + " "
     return puzzleprint
 
