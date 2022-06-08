d2194c5b02cf6d954e09dbea0a9247a84bf86a41
romakouz <67249384+romakouz@users.noreply.github.com>
2022-06-07 23:55:01 -0700

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -36,103 +36,95 @@ def submit():
     if request.method == 'GET':
         return render_template('submit.html')
     else:
-        img = request.files['image'] #get image as jpg object
         
-        if not img:
-            #if no image,  then user is only submitting correction
-            corr = request.form['correction']
-            #flash('Thanks for the correction!')
             
-            return render_template('submit.html', adjusting=True, adjustment = corr)
-        else:
-                
+        try:            
+            '''
+            1. Access the image
+            2. Load the pickled ML model
+            3. Run the ML model on the image
+            4. Store the ML model's prediction in some Python variable
+            5. Show the image on the template
+            6. Print the prediction and some message on the template
+            '''
+            # 1
+            img = request.files['image'] #get image as jpg object
+
+            error1 =False
+            error2 =False
+            error3 =False
+
+
             
-            try:            
-                '''
-                1. Access the image
-                2. Load the pickled ML model
-                3. Run the ML model on the image
-                4. Store the ML model's prediction in some Python variable
-                5. Show the image on the template
-                6. Print the prediction and some message on the template
-                '''
-                # 1
+            try:
+                img = Image.open(img)
+                gray_img = img.convert("L")
+                img = np.array(gray_img)
+            except:
+                error1 = True
+                return render_template('submit.html', error1=True)
+            #np.loadtxt(img) # numpy array with the pixel values
+            img_shape = img.shape
+            
+            # 2 load pickle model, call CompleteSudokuPredictFromRaw to get prediction of puzzle from the model
+            
 
-                error1 =False
-                error2 =False
-                error3 =False
-
-
-                
-                try:
-                    img = Image.open(img)
-                    gray_img = img.convert("L")
-                    img = np.array(gray_img)
-                except:
-                    error1 = True
-                    return render_template('submit.html', error1=True)
-                #np.loadtxt(img) # numpy array with the pixel values
-                img_shape = img.shape
-                
-                # 2 load pickle model, call CompleteSudokuPredictFromRaw to get prediction of puzzle from the model
-                
-
-                try:
-                    model = pickle.load(open('sudoku-model/model.pkl', 'rb'))
-                
-                    
-                except:
-                    return render_template('submit.html', error2=True)
-                # 5. this code is for displaying an image, we want to print a numpy array 
-                #fig = Figure(figsize=(3, 3))
-                #ax = fig.add_subplot(1, 1, 1,)
-                #ax.imshow(img, cmap='binary')
-                #ax.axis("off")
-
-                # weird part of 5. 
-                #pngImage = io.BytesIO()
-                #FigureCanvas(fig).print_png(pngImage) # convert the pyplot figure object to a PNG image
-
-                # encode the PNG image to base64 string
-                #pngImageB64String = "data:image/png;base64,"
-                #pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
-
-                #instead, create string for predicted puzzle, and solution
-                puzzle, uncertain_values = pnp.CompleteSudokuPredictFromRaw(img, model)
-                
-                puzzle_str = pf.print_puzzle(puzzle)
-
-                if len(uncertain_values) > 0:
-                    uncertain_str = "We are uncertain if our prediction(s) in"
-                    for i in range(len(uncertain_values)):
-                        #add comma if not first value
-                        if i > 0:
-                            uncertain_str += ","
-                        
-                        cell = uncertain_values[i]
-                        uncertain_str += " row " + str(cell[0]) + ", column " + str(cell[1])
-                        
-                        
-                    uncertain_str += " is correct.\n Please double check this with the image."
-
-                    return render_template('submit.html', uncertainty=True, prediction=puzzle_str, uncertain=uncertain_str)
-                
-                else:
-                    
-
-                    #compute solution
-                    puzzle_sol = puzzle.copy()
-                    try:
-                        #try to solve the puzzle
-                        sf.sudoku_solve(puzzle_sol)
-                        puzzle_sol_str = pf.print_puzzle(puzzle_sol)
-                        return render_template('submit.html', prediction=puzzle_str, solution=puzzle_sol_str)
-                    except:
-                        #if cannot solve, only return prediction
-                        return render_template('submit.html', prediction=puzzle_str, solve_error = True)
+            try:
+                model = pickle.load(open('sudoku-model/model.pkl', 'rb'))
+            
                 
             except:
-                return render_template('submit.html', error=True)
+                return render_template('submit.html', error2=True)
+            # 5. this code is for displaying an image, we want to print a numpy array 
+            #fig = Figure(figsize=(3, 3))
+            #ax = fig.add_subplot(1, 1, 1,)
+            #ax.imshow(img, cmap='binary')
+            #ax.axis("off")
+
+            # weird part of 5. 
+            #pngImage = io.BytesIO()
+            #FigureCanvas(fig).print_png(pngImage) # convert the pyplot figure object to a PNG image
+
+            # encode the PNG image to base64 string
+            #pngImageB64String = "data:image/png;base64,"
+            #pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+
+            #instead, create string for predicted puzzle, and solution
+            puzzle, uncertain_values = pnp.CompleteSudokuPredictFromRaw(img, model)
+            
+            puzzle_str = pf.print_puzzle(puzzle)
+
+            if len(uncertain_values) > 0:
+                uncertain_str = "We are uncertain if our prediction(s) in"
+                for i in range(len(uncertain_values)):
+                    #add comma if not first value
+                    if i > 0:
+                        uncertain_str += ","
+                    
+                    cell = uncertain_values[i]
+                    uncertain_str += " row " + str(cell[0]) + ", column " + str(cell[1])
+                    
+                    
+                uncertain_str += " is correct.\n Please double check this with the image."
+
+                return render_template('submit.html', uncertainty=True, prediction=puzzle_str, uncertain=uncertain_str)
+            
+            else:
+                
+
+                #compute solution
+                puzzle_sol = puzzle.copy()
+                try:
+                    #try to solve the puzzle
+                    sf.sudoku_solve(puzzle_sol)
+                    puzzle_sol_str = pf.print_puzzle(puzzle_sol)
+                    return render_template('submit.html', prediction=puzzle_str, solution=puzzle_sol_str)
+                except:
+                    #if cannot solve, only return prediction
+                    return render_template('submit.html', prediction=puzzle_str, solve_error = True)
+            
+        except:
+            return render_template('submit.html', error=True)
 
 @app.route('/correcting', methods=['POST'])
 def correcting():
