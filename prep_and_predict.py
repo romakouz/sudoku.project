355aae433b46a7b2d9f66267e82eb7948f104165
romakouz <67249384+romakouz@users.noreply.github.com>
2022-06-07 22:01:20 -0700

    fix

--- prep_and_predict.py
+++ prep_and_predict.py
@@ -173,7 +173,7 @@ def CompleteSudokuPredictFromRaw(image, model_pickled):
     for j in range(9):
       prediction, certainty = CNN1predict(cells[i*9 +j], model_pickled)
       puzzle_arr[i][j]=prediction
-      if certainty < 0.8: 
+      if certainty < 0.7: 
         #add uncertain tuple to list
         uncertain.append((i,j))
         #print("We are uncertain if the prediction in row " + i.str() + ", column "+ j.str() +" is correct.")
